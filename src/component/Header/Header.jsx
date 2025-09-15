17b6b3d742083a2344cb447aa1029d5e3b6aa36e
rajalishikhe <nahidujjamanraj@gmail.com>
2025-09-15 10:45:39 +0600

    Now I am provider the authontication

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div >
-            <Link to={'/'}>Home</Link>
-            <Link to={'/Dashboard'}>DashBoard</Link>
-            <Link to={"/login"}>Login</Link>
+        <div className='mt-2 mb-2' >
+            <Link className='ml-2 p-2 bg-orange-300 text-black rounded-xl hover:bg-red-500 hover:text-white' to={'/'}>Home</Link>
+            <Link  className='ml-2 p-2 bg-orange-300 text-black rounded-xl hover:bg-red-500 hover:text-white' to={'/Dashboard'}>DashBoard</Link>
+            <Link className='ml-2 p-2 bg-orange-300 text-black rounded-xl hover:bg-red-500 hover:text-white' to={"/login"}>Login</Link>
             
         </div>
     );
--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,10 +1,19 @@
-
-console.log 
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../firebase/Firebase";
 
 const Login = () => {
+    const provider = new GoogleAuthProvider();
+    const handleClick=()=>{
+        console.log ("Login with google")
+        signInWithPopup(auth,provider).then((result)=>{
+            console.log(result)
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
     return (
         <div>
-            <button className="text-2xl">Login with google</button>
+            <button  onClick={handleClick} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700">Login with google</button>
             
         </div>
     );

4abd0922db191a532797ae13bdbc7a8d16aed357
rajalishikhe <nahidujjamanraj@gmail.com>
2025-09-17 09:49:53 +0600

    Now I am show the details of my email

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,12 +1,15 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 import auth from "../../firebase/Firebase";
 
 const Login = () => {
+    const [user,setUser]=useState(null)
     const provider = new GoogleAuthProvider();
     const handleClick=()=>{
         console.log ("Login with google")
         signInWithPopup(auth,provider).then((result)=>{
             console.log(result)
+            setUser(result.user)
         }).catch((error)=>{
             console.log(error)
         })
@@ -14,6 +17,12 @@ const Login = () => {
     return (
         <div>
             <button  onClick={handleClick} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700">Login with google</button>
+          
+            {
+            user && <div><p>Name:{user.displayName}</p>
+           <p>Email:{user.email}</p> </div> 
+          }
+          
             
         </div>
     );

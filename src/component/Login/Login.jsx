4c8a5f8d426360d1ced778a84ddb6c6b2bd2097b
rajalishikhe <nahidujjamanraj@gmail.com>
2025-09-17 09:56:13 +0600

    Now I am  add logout method in my website

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
 import auth from "../../firebase/Firebase";
 
@@ -14,9 +14,21 @@ const Login = () => {
             console.log(error)
         })
     }
+    const handleLogout=()=>{
+        setUser(null)
+        signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  console.log(error)
+});
+    }
     return (
         <div>
-            <button  onClick={handleClick} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700">Login with google</button>
+            
+            {
+                user?  <button onClick={handleLogout} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700" >LogOut</button>:
+                <button  onClick={handleClick} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700">Login with google</button>
+            }
           
             {
             user && <div><p>Name:{user.displayName}</p>

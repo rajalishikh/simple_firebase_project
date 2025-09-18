64ead2e77011eb67eb5d4dd35fe3eb43f170da29
rajalishikhe <nahidujjamanraj@gmail.com>
2025-09-18 09:48:36 +0600

    Now I am Show add the github authentication system in my web page

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,10 +1,11 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
 import auth from "../../firebase/Firebase";
 
 const Login = () => {
     const [user,setUser]=useState(null)
     const provider = new GoogleAuthProvider();
+    // login with google 
     const handleClick=()=>{
         console.log ("Login with google")
         signInWithPopup(auth,provider).then((result)=>{
@@ -13,6 +14,23 @@ const Login = () => {
         }).catch((error)=>{
             console.log(error)
         })
+    }
+    // login with github 
+    const providerGithub = new GithubAuthProvider();
+    const handleClickGithub=()=>{
+        console.log("login  Successful with github")
+        signInWithPopup(auth, providerGithub)
+  .then((result) => {
+    console.log(result)
+    setUser(result.user)
+    
+
+    
+  }).catch((error) => {
+    console.log(error)
+    
+   
+  });
     }
     const handleLogout=()=>{
         setUser(null)
@@ -27,7 +45,10 @@ const Login = () => {
             
             {
                 user?  <button onClick={handleLogout} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700" >LogOut</button>:
-                <button  onClick={handleClick} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700">Login with google</button>
+                <div>
+                    <button  onClick={handleClick} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700 mr-2">Login with google</button>
+                    <button  onClick={handleClickGithub} className="text-2xl p-2 bg-green-600 text-white rounded-xl hover:bg-red-700">Login with github</button>
+                </div>
             }
           
             {
